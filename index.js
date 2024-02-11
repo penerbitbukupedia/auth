19b7a09152f0ccd311ac90e4c281f327963f499d
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-11 08:09:54 +0700

    hemat tempat dan variabel

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import {getWithHeader} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 
-const login = getCookie("login");
-getWithHeader("https://ped.fly.dev/auth/phonenumber","login",login,responseFunction);
+getWithHeader("https://ped.fly.dev/auth/userdata","login",getCookie("login"),responseFunction);
 
 function responseFunction(result){
     console.log(result.phone);
