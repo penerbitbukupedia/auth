2c97a9046a2d013b48f1d43e58c3e1851ed86dff
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-11 09:21:02 +0700

    ok

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import {getWithHeader} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
+import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
-getWithHeader("https://ped.fly.dev/auth/userdata","login",getCookie("login"),responseFunction);
+if (getCookie("login")){
+    getWithHeader("https://ped.fly.dev/auth/userdata","login",getCookie("login"),responseFunction);
+}else{
+    redirect("../");
+}
+
 
 function responseFunction(result){
     console.log(result.phone);
